8b449c39f200c7d1b5fa262eb2cd1a83669f2f4f
satiwari <s.t.48826@gmail.com>
2023-05-07 16:09:46 +0530

    router file setup

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-
+// routing for this app
+app.use('/', require('./routes'));
 
 
 
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const route = express.Router();
+
+console.log('router loaded');
+
+
+module.exports = route;
